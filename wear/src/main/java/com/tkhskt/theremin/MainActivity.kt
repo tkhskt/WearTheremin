5ab15204d4355db38527a9e4b07913024a28f5e2
tkhskt <luckymortality@gmail.com>
2022-04-19 02:20:46 +0900

    disable ambient

--- wear/src/main/java/com/tkhskt/theremin/MainActivity.kt
+++ wear/src/main/java/com/tkhskt/theremin/MainActivity.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.FragmentActivity
-import androidx.wear.ambient.AmbientModeSupport
 import com.tkhskt.theremin.ui.MainScreen
 import com.tkhskt.theremin.ui.MainViewModel
 import com.tkhskt.theremin.ui.model.MainEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : FragmentActivity(), AmbientModeSupport.AmbientCallbackProvider {
+class MainActivity : FragmentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
-
-    private lateinit var ambientController: AmbientModeSupport.AmbientController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +22,5 @@ class MainActivity : FragmentActivity(), AmbientModeSupport.AmbientCallbackProvi
             }
         })
         viewModel.dispatchEvent(MainEvent.Initialize)
-        ambientController = AmbientModeSupport.attach(this)
-    }
-
-    override fun getAmbientCallback(): AmbientModeSupport.AmbientCallback = ThereminAmbientCallback()
-
-    private class ThereminAmbientCallback : AmbientModeSupport.AmbientCallback() {
-
-        override fun onEnterAmbient(ambientDetails: Bundle?) {
-            // Handle entering ambient mode
-        }
-
-        override fun onExitAmbient() {
-            // Handle exiting ambient mode
-        }
-
-        override fun onUpdateAmbient() {
-            // Update the content
-        }
     }
 }
