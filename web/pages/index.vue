ba49ead15a1839c843d4cd76f3b3f9c81d66f643
tkhskt <luckymortality@gmail.com>
2022-05-04 23:54:47 +0900

    move native projects

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -34,29 +34,29 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-const UUID_ANDROID_SERVICE = "a9d158bb-9007-4fe3-b5d2-d3696a3eb067";
-const UUID_ANDROID_NOTIFY = "52dc2803-7e98-4fc2-908a-66161b5959b0";
+import { mapState } from 'vuex'
+const UUID_ANDROID_SERVICE = 'a9d158bb-9007-4fe3-b5d2-d3696a3eb067'
+const UUID_ANDROID_NOTIFY = '52dc2803-7e98-4fc2-908a-66161b5959b0'
 
-const ANDROID_WAIT = 200;
+const ANDROID_WAIT = 200
 
-let bluetoothDevice = null;
-const characteristics = new Map();
+let bluetoothDevice = null
+const characteristics = new Map()
 
 export default {
-  name: "IndexPage",
-  el: "#top",
+  name: 'IndexPage',
+  el: '#top',
   data() {
     return {
-      progress_title: "",
+      progress_title: '',
 
       ble_connected: false,
-      ble_devicename: "",
-      ble_notify_value: "",
-    };
+      ble_devicename: '',
+      ble_notify_value: '',
+    }
   },
   computed: {
-    ...mapState("main", ["frequency"]),
+    ...mapState('main', ['frequency']),
   },
   mounted() {
     // window.onmousemove = (e) => {
@@ -68,131 +68,129 @@ export default {
     ble_connect() {
       return this.requestDevice(UUID_ANDROID_SERVICE)
         .then((name) => {
-          this.ble_devicename = name;
+          this.ble_devicename = name
           return bluetoothDevice.gatt
             .connect()
             .then((server) => {
-              console.log("Execute : getPrimaryService");
+              console.log('Execute : getPrimaryService')
               return waitAsync(ANDROID_WAIT).then(() => {
-                return server.getPrimaryService(UUID_ANDROID_SERVICE);
-              });
+                return server.getPrimaryService(UUID_ANDROID_SERVICE)
+              })
             })
             .then((service) => {
-              console.log("Execute : getCharacteristic");
-              characteristics.clear();
+              console.log('Execute : getCharacteristic')
+              characteristics.clear()
               return Promise.all([
                 this.setCharacteristic(service, UUID_ANDROID_NOTIFY),
-              ]);
+              ])
             })
             .then((values) => {
               return waitAsync(ANDROID_WAIT).then(() => {
-                return this.startNotify(UUID_ANDROID_NOTIFY);
-              });
+                return this.startNotify(UUID_ANDROID_NOTIFY)
+              })
             })
             .then(() => {
-              this.ble_connected = true;
-              console.log("ble_connect done");
-              return bluetoothDevice.name;
+              this.ble_connected = true
+              console.log('ble_connect done')
+              return bluetoothDevice.name
             })
             .catch((error) => {
-              alert(error);
+              alert(error)
             })
             .finally(() => {
               // this.progress_close()
-            });
+            })
         })
         .catch((error) => {
-          alert(error);
-        });
+          alert(error)
+        })
     },
     requestDevice(serviceUUID) {
-      console.log("Execute : requestDevice");
+      console.log('Execute : requestDevice')
 
       return navigator.bluetooth
         .requestDevice({
           filters: [{ services: [serviceUUID] }],
         })
         .then((device) => {
-          console.log("requestDevice OK");
-          characteristics.clear();
-          bluetoothDevice = device;
+          console.log('requestDevice OK')
+          characteristics.clear()
+          bluetoothDevice = device
           bluetoothDevice.addEventListener(
-            "gattserverdisconnected",
+            'gattserverdisconnected',
             this.onDisconnect
-          );
-          return bluetoothDevice.name;
-        });
+          )
+          return bluetoothDevice.name
+        })
     },
     setCharacteristic(service, characteristicUuid) {
-      console.log("Execute : setCharacteristic : " + characteristicUuid);
+      console.log('Execute : setCharacteristic : ' + characteristicUuid)
 
       return waitAsync(ANDROID_WAIT)
         .then(() => {
-          return service.getCharacteristic(characteristicUuid);
+          return service.getCharacteristic(characteristicUuid)
         })
         .then((characteristic) => {
-          characteristics.set(characteristicUuid, characteristic);
+          characteristics.set(characteristicUuid, characteristic)
           characteristic.addEventListener(
-            "characteristicvaluechanged",
+            'characteristicvaluechanged',
             this.onDataChanged
-          );
-          return service;
-        });
+          )
+          return service
+        })
     },
     onDisconnect(event) {
-      console.log("onDisconnect");
-      characteristics.clear();
-      this.ble_connected = false;
+      console.log('onDisconnect')
+      characteristics.clear()
+      this.ble_connected = false
     },
     onDataChanged(event) {
-      const characteristic = event.target;
+      const characteristic = event.target
 
       if (characteristic.uuid === UUID_ANDROID_NOTIFY) {
-        const values = new TextDecoder()
-          .decode(characteristic.value)
-          .split(",");
-        const freq = parseFloat(values[0]);
-        const volume = parseFloat(values[1]);
+        const values = new TextDecoder().decode(characteristic.value).split(',')
+        const freq = parseFloat(values[0])
+        const volume = parseFloat(values[1])
         // const distance = values[1]
         // const newFreq = this.calcFrequency(parseFloat(gravity))
-        this.$store.dispatch("main/onChangeFrequency", freq);
-        this.$store.dispatch("main/onChangeVolume", volume);
-        this.ble_notify_value = freq;
+        this.$store.dispatch('main/onChangeFrequency', freq)
+        this.$store.dispatch('main/onChangeVolume', volume)
+        this.ble_notify_value = freq
       }
     },
     startNotify(uuid) {
       if (characteristics.get(uuid) === undefined) {
-        console.log("Not Connected");
-        return;
+        console.log('Not Connected')
+        return
       }
 
-      console.log("Execute : startNotifications");
-      return characteristics.get(uuid).startNotifications();
+      console.log('Execute : startNotifications')
+      return characteristics.get(uuid).startNotifications()
     },
     stopNotify(uuid) {
       if (characteristics.get(uuid) === undefined) {
-        console.log("Not Connected");
-        return;
+        console.log('Not Connected')
+        return
       }
 
-      console.log("Execute : stopNotifications");
-      return characteristics.get(uuid).stopNotifications();
+      console.log('Execute : stopNotifications')
+      return characteristics.get(uuid).stopNotifications()
     },
     calcFrequency(gravity) {
-      if (Number.isNaN(gravity)) return this.frequency;
+      if (Number.isNaN(gravity)) return this.frequency
       // if (Math.abs(acceleration) < 1) return this.frequency
-      const newFreq = 269.292 + (gravity + 4) * 50.49;
-      if (newFreq < 269.292) return this.frequency;
-      if (newFreq > 1077.167) return this.frequency;
-      return newFreq;
+      const newFreq = 269.292 + (gravity + 4) * 50.49
+      if (newFreq < 269.292) return this.frequency
+      if (newFreq > 1077.167) return this.frequency
+      return newFreq
     },
     calcVolume(distance) {},
   },
-};
+}
 
 function waitAsync(timeout) {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, timeout);
-  });
+    setTimeout(resolve, timeout)
+  })
 }
 </script>
