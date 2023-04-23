90c65875442181dd5ec3de0ab41de87321a00087
tkhskt <luckymortality@gmail.com>
2023-04-24 00:27:45 +0900

    add: infra module

---
+++ native/infra/build.gradle.kts
@@ -0,0 +1,59 @@
+val ktlint: Configuration by configurations.creating
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 30
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+    }
+}
+
+apply(from = "$rootDir/gradle/ktlint.gradle")
+
+dependencies {
+
+    // hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // Coroutines
+    implementation(libs.bundles.coroutines)
+
+    implementation(libs.timber)
+    ktlint(libs.ktlint)
+
+    implementation(libs.bundles.moshi)
+
+    testImplementation(libs.test.junit)
+    androidTestImplementation(libs.test.junit.ext)
+    androidTestImplementation(libs.test.espresso)
+}
