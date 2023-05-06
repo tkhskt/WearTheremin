3de803e7126c326788f817211c1db1db3de0faf0
tkhskt <luckymortality@gmail.com>
2023-05-07 02:39:00 +0900

    fix: tutorial animation

--- native/feature-tutorial/src/main/java/com/tkhskt/theremin/feature/tutorial/ui/component/TutorialContent.kt
+++ native/feature-tutorial/src/main/java/com/tkhskt/theremin/feature/tutorial/ui/component/TutorialContent.kt
@@ -1,21 +1,14 @@
 package com.tkhskt.theremin.feature.tutorial.ui.component
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.tkhskt.theremin.core.ui.ThereminTheme
 
@@ -25,36 +18,22 @@ fun TutorialContent(
     body: String,
     visible: Boolean,
     modifier: Modifier = Modifier,
-    initialOffsetY: Float = 50f,
 ) {
-    val density = LocalDensity.current
     Box(
         modifier = modifier,
     ) {
         AnimatedVisibility(
             visible = visible,
-            enter = slideInVertically(
-                animationSpec = tween(durationMillis = 400),
-            ) {
-                with(density) { initialOffsetY.dp.roundToPx() }
-            } + fadeIn(),
-            exit = slideOutVertically(
-                animationSpec = tween(durationMillis = 200),
-            ) {
-                with(density) { initialOffsetY.dp.roundToPx() }
-            } + fadeOut(),
+            enter = fadeIn(),
+            exit = fadeOut(),
         ) {
-            Column(
-                modifier = Modifier
-                    .wrapContentHeight()
-                    .fillMaxWidth(),
-            ) {
+            Column {
                 Text(
                     text = title,
                     style = ThereminTheme.typography.headlineMedium,
                 )
-                Spacer(modifier = Modifier.size(36.dp))
                 Text(
+                    modifier = Modifier.padding(vertical = 32.dp),
                     text = body,
                     style = ThereminTheme.typography.bodyMedium,
                 )
