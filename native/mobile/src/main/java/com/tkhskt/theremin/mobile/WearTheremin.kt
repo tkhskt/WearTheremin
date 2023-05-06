9f3ea0d7fc539a55839fd00e5c9a3f6d25735dd1
tkhskt <luckymortality@gmail.com>
2023-05-07 02:04:53 +0900

    remove unused import

--- native/mobile/src/main/java/com/tkhskt/theremin/mobile/WearTheremin.kt
+++ native/mobile/src/main/java/com/tkhskt/theremin/mobile/WearTheremin.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.hilt.navigation.compose.hiltViewModel
